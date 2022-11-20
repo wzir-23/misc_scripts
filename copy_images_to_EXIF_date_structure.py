c9f0f215a98db76675e29f5e938543d4c749ce4a
wzir-23 <wzir@borken.se>
2022-11-20 17:16:10 +0100

    Made a time sacrifice to pylint

--- copy_images_to_EXIF_date_structure.py
+++ copy_images_to_EXIF_date_structure.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+''' Recursively searches for movies and images in a directory (including
+    sub-directories and copies them to file structures on the format:
+
+    images/YYYY/MM/DD
+    movies/YYYY/MM/DD
+
+    Usage:
+    ./copy_images_to_EXIF_date_structure.py <image_folder>
+'''
 
 import shutil
 import subprocess
@@ -11,12 +20,15 @@ MOVIES = 2
 
 # Filmformat: .mov, .mp4, .3gp,
 def find_image_files(filepath):
+    ''' searches for images and movies in the supplied filepath based on file
+        suffix
+    '''
     img_files = []
     mov_files = []
     file_ext = ['.xmp', '.jpg', '.JPG', '.heic', '.HEIC',
                 '.dng', '.DNG', '.PNG', '.RAF']
     mov_ext = ['.mov', '.MOV', '.mp4', '.3gp', '.MP4']
-    for dirpath, dirnames, filenames in os.walk(filepath):
+    for dirpath, _, filenames in os.walk(filepath):
         for fname in filenames:
             if fname.endswith(tuple(file_ext)):
                 img_files.append(os.path.join(dirpath, fname))
@@ -30,17 +42,20 @@ def find_image_files(filepath):
 
 
 def new_file_path(img_files, FILE_TYPE):
+    ''' set destination path for images/movies and supplies it to copy_file()
+    '''
     if FILE_TYPE == IMAGES:
         prefix = 'images'
     else:
         prefix = 'movies'
     for fname in img_files:
         year, month, day = find_creation_date(fname, prefix)
-        destination = "{}/{}/{}/{}/".format(prefix, year, month, day)
+        destination = f'{prefix}/{year}/{month}/{day}/'
         copy_file(fname, destination)
 
 
 def find_creation_date(fname, prefix):
+    ''' retrieves creation date for supplied object '''
     print(fname)
     process = subprocess.Popen(["exiftool", fname],
                                stdout=subprocess.PIPE,
@@ -52,37 +67,40 @@ def find_creation_date(fname, prefix):
         match_str = 'Date/Time Original'
     else:
         match_str = 'Creation Date'
-    for x in exif_output:
-        if match_str in x:
+    for exif in exif_output:
+        if match_str in exif:
             # x = ['Date/Time Original              ', '2022:05:08 11:24:37']
             # date_time = '2022:05:08 11:24:37'
-            date_time = x.split(': ')[1]
+            date_time = exif.split(': ')[1]
             break
     if 'date_time' in locals():
         date_str = date_time.split(' ')[0]
-        return date_str.split(':')
     else:  # we have a picture that is not a photo
-        for x in exif_output:
-            if 'File Modification Date/Time' in x:
-                date_time = x.split(': ')[1]
+        for exif in exif_output:
+            if 'File Modification Date/Time' in exif:
+                date_time = exif.split(': ')[1]
                 date_str = date_time.split(' ')[0]
                 break
     return date_str.split(':')
 
 
-def create_dir(f):
-    new_dir = os.path.dirname(f)
+def create_dir(directory):
+    ''' creates new directory if needed '''
+    new_dir = os.path.dirname(directory)
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
 
 
 def copy_file(fname, destination):
+    ''' copies files '''
     create_dir(destination)
     shutil.copy2(fname, destination)
     copy_meta_file(fname, destination)
 
 
 def copy_meta_file(fname, destination):
+    ''' Handle metafiles from Exposure software X5. Should be updated to cover
+        all later versions'''
     meta_path_prefix = 'Exposure Software/Exposure X5'
     meta_path = os.path.join(os.path.dirname(fname), meta_path_prefix) + '/'
     meta_file = meta_path + os.path.split(fname)[1] + '.exposurex5'
@@ -93,10 +111,12 @@ def copy_meta_file(fname, destination):
 
 
 def main():
+    ''' Recursively search and copy image files to a date based file structure
+    '''
     img_files, mov_files = find_image_files(sys.argv[1])
-    if len(img_files):
+    if img_files:
         new_file_path(img_files, IMAGES)
-    if len(mov_files):
+    if mov_files:
         new_file_path(mov_files, MOVIES)
 
 
--- todo.py
+++ todo.py
@@ -1,77 +1,82 @@
 #!/usr/bin/env python3
-
+''' Command line TODO tool that stores a list of tasks in ~/.todo.txt that
+    is manipulated via a(dd), d(elete) and l(ist) arguments. Supply -h or
+    --help for usage information
+'''
 import os
 import sys
 
 
 def print_usage(todo_script):
-    print('\nusage: %s [-had] text' % todo_script)
+    ''' print usage information '''
+    print(f'\nusage: {todo_script} [-had] text')
     print('\ta "text to add     # add task')
     print('\td <number>         # delete task')
     print('\tl (or no argument  # list tasks\n')
 
 
-def handle_arguments(args):
+def handle_arguments(fname, args):
+    ''' simple argument handling '''
     if len(args) == 1:          # no args, all
-        read_file()
+        read_file(fname)
         return
     if args[1] == '-h' or args[1] == '--help' or len(args) > 3:
         print_usage(args[1])
         return
     if args[1] == 'l':          # list all
-        read_file()
+        read_file(fname)
     if args[1] == 'a':          # add entry
-        add_entry(args[2])
+        add_entry(fname, args[2])
     if args[1] == 'd':          # remove entry
-        delete_entry(args[2])
+        delete_entry(fname, args[2])
     return
 
 
-def read_file():
-    fname = '~/.todo.txt'
-    fname = os.path.expanduser(fname)
+def read_file(fname):
+    ''' read TODO file and print contents '''
     if os.path.isfile(fname):
-        with open(fname,'r') as fhandle:
+        with open(fname,'r', encoding='UTF-8') as fhandle:
             entries = fhandle.read().splitlines()
         for count, msg in enumerate(entries, start=1):
             print(f'{count:<3} {msg}')
 
 
-def add_entry(msg):
-    fname = '~/.todo.txt'
-    fname = os.path.expanduser(fname)
-    os.path.isfile(fname)
-    fhandle = open(fname,'a')
-    fhandle.write(msg + '\n')
-    fhandle.close()
+def add_entry(fname, msg):
+    ''' add a new item to TODO list '''
+    mode = 'w'
+    if os.path.isfile(fname):
+        mode = 'a'
+    with open(fname, mode, encoding='UTF-8') as fhandle:
+        fhandle.write(msg + '\n')
     print(f'Added: {msg}')
 
 
-def delete_entry(number):
-    fname = '~/.todo.txt'
-    fname = os.path.expanduser(fname)
+def delete_entry(fname, number):
+    ''' remove item from TODO list '''
     new_todos = []
-    if not number.isnumeric():
-        return False
-    number = int(number)
-    if os.path.isfile(fname):
-        with open(fname,'r') as fhandle:
-            entries = fhandle.read().splitlines()
-        for count, msg in enumerate(entries, start=1):
-            if count == number:
-                print(f'Removed {msg}')
-            else:
-                new_todos.append(msg)
-    if new_todos:
-        with open(fname, 'w') as fhandle:
-            for msg in new_todos:
-                fhandle.write(msg + '\n')
-    else:
-        os.remove(fname)
+    if number.isnumeric():
+        number = int(number)
+        if os.path.isfile(fname):
+            with open(fname, 'r', encoding='UTF-8') as fhandle:
+                entries = fhandle.read().splitlines()
+            for count, msg in enumerate(entries, start=1):
+                if count == number:
+                    print(f'Removed {msg}')
+                else:
+                    new_todos.append(msg)
+        if new_todos:
+            with open(fname, 'w', encoding='UTF-8') as fhandle:
+                for msg in new_todos:
+                    fhandle.write(msg + '\n')
+        else:
+            os.remove(fname)
 
 
 def main():
-    handle_arguments(sys.argv)
+    ''' program main function '''
+    fname = '~/.todo.txt'
+    fname = os.path.expanduser(fname)
+    handle_arguments(fname, sys.argv)
 
 
 if __name__ == "__main__":
