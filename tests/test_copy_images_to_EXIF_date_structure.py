533bd4156d62ffcc221f2c98a7f72a456daae565
wzir-23 <wzir@borken.se>
2024-08-04 16:01:43 +0200

    added more tests

--- tests/test_copy_images_to_EXIF_date_structure.py
+++ tests/test_copy_images_to_EXIF_date_structure.py
@@ -12,3 +12,17 @@ class TestFindCreationDate(unittest.TestCase):
         photo = "tests/test_images/IMG_0830.JPG"
         self.assertEqual(copy_images.find_creation_date(photo, 'images'), 
                          ['2022', '03', '13'])
+
+    def test_name_collision(self):
+        """ test create unique file names """
+        example_file = 'tests/test_images/subfolder/IMG_0693.MOV'
+        example_path = 'tests/test_images/subfolder'
+        self.assertEqual(copy_images.unique_fname(example_file, example_path),
+                         'IMG_0693_1.MOV')
+
+    def test_file_type_identification(self):
+        """ test identify images and movies """
+        example_path = 'tests/test_images/subfolder'
+        self.assertEqual(copy_images.find_image_files(example_path),
+                         (['tests/test_images/subfolder/IMG_0049.jpg'],
+                         ['tests/test_images/subfolder/IMG_0693.MOV']))
